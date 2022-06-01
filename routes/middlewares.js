db28e60295dca2eeb37764896a0f26452cb00161
SeungheonShin <75475398+SeungheonShin@users.noreply.github.com>
2022-06-01 16:37:10 +0900

    fix res error

--- routes/middlewares.js
+++ routes/middlewares.js
@@ -7,27 +7,27 @@ module.exports = {
         if (req.headers.authorizationtoken) {
           next();
         } else {
-          res.status(403).json({"message" : "로그인 필요"});
+          return res.status(403).json({"message" : "로그인 필요"});
         }
     },
     isNotLoggedIn : (req, res, next) => {
         if (!req.headers.authorization) {
             next();
         } else {
-            res.status(403).json({"message" : "로그아웃 필요"});
+            return res.status(403).json({"message" : "로그아웃 필요"});
         }
     },
     verifyToken : async (req, res, next) => {
         const bearerHeader = req.headers['authorization'];
         if(bearerHeader === undefined) {
-            res.status(403).json({"message" : "토큰 정보가 없습니다."});
+            return res.status(403).json({"message" : "토큰 정보가 없습니다."});
         }
 
         const token = bearerHeader.split(' ')[1];
         await jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, authData) => {
             if(err) {
                 console.error(err);
-                res.status(403).json({"message" : "토큰 정보가 오염되었습니다."});
+                return res.status(403).json({"message" : "토큰 정보가 오염되었습니다."});
                 
             }
             const userId = authData.userId;
@@ -42,7 +42,7 @@ module.exports = {
                     req.body.userId = user.id;
                 }
                 else {
-                    res.status(403).json({"message" : "아이디가 존재하지 않습니다."});
+                    return res.status(403).json({"message" : "아이디가 존재하지 않습니다."});
                 }
             });
             } catch(err) {
